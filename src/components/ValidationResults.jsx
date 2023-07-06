a740c364adba55d204daed04a3b37915b556a254
Mathias Rechtzigel <135732820+Mathias-CMS@users.noreply.github.com>
2023-07-06 16:45:51 -0500

    feat: adding skeleton loader (#2)

--- src/components/ValidationResults.jsx
+++ src/components/ValidationResults.jsx
@@ -30,62 +30,113 @@ const ValidationResults = ({
         <h2 id="validation-results-header" tabIndex="-1" ref={resultsHeaderRef}>
           Validation results
         </h2>
-        {loading && (
-          <p className="font-sans-xl text-bold">Your file is processing...</p>
-        )}
-        {!loading && filename && (
-          <>
-            <h3>Filename</h3>
-            <Alert
-              type={filenameValid ? `success` : `error`}
-              aria-live="polite"
-              aria-atomic="true"
-            >
-              {filenameValid ? (
+        <div id="validation-results-body">
+          {loading && (
+              <p className="font-sans-l loading-skeleton">Your file is processing. Some larger files may take a minute or two. Results will appear here once completed. If you are experiencing issues, please let us know at : <a href="mailto:PriceTransparencyHospitalCharges@cms.hhs.gov">PriceTransparencyHospitalCharges@cms.hhs.gov</a>.</p>
+          )}
+          {!loading && filename && (
+            <>
+              <h3>Filename</h3>
+              <Alert
+                type={filenameValid ? `success` : `error`}
+                aria-live="polite"
+                aria-atomic="true"
+              >
+                {filenameValid ? (
+                  <>
+                    <span className="text-bold">Filename valid</span>:{" "}
+                    <span className="text-underline">{filename}</span>
+                  </>
+                ) : (
+                  <>
+                    <span className="text-bold">Filename invalid</span>:{" "}
+                    <span className="text-underline">{filename}</span>
+                    <br />
+                    <span>
+                      Must match format:
+                      &lt;ein&gt;_&lt;hospitalname&gt;_standardcharges.[json|csv].{" "}
+                      <a href={`${import.meta.env.BASE_URL}filename-wizard`}>
+                        Click here to use the file name wizard.
+                      </a>
+                    </span>
+                  </>
+                )}
+              </Alert>
+              <h3>Errors</h3>
+              <Alert
+                type={valid ? `success` : `error`}
+                aria-live="polite"
+                aria-atomic="true"
+              >
+                {valid ? (
+                  <>
+                    <span className="text-bold">No errors found in file</span>:{" "}
+                    <span className="text-underline">{filename}</span>
+                  </>
+                ) : (
+                  <>
+                    <span className="text-bold">
+                      {errors.length === 1
+                        ? "1 error"
+                        : `${errors.length} errors`}{" "}
+                      found in file
+                    </span>
+                    : <span className="text-underline">{filename}</span>
+                  </>
+                )}
+              </Alert>
+              {errors.length > 0 && (
                 <>
-                  <span className="text-bold">Filename valid</span>:{" "}
-                  <span className="text-underline">{filename}</span>
-                </>
-              ) : (
-                <>
-                  <span className="text-bold">Filename invalid</span>:{" "}
-                  <span className="text-underline">{filename}</span>
-                  <br />
-                  <span>
-                    Must match format:
-                    &lt;ein&gt;_&lt;hospitalname&gt;_standardcharges.[json|csv].{" "}
-                    <a href={`${import.meta.env.BASE_URL}filename-wizard`}>
-                      Click here to use the file name wizard.
+                  <Table className="width-full" bordered striped>
+                    <thead>
+                      <tr>
+                        <th scope="col">{locationHeader}</th>
+                        <th scope="col">Error description</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {errors.map(({ path, message }, index) => (
+                        <tr key={index}>
+                          <td>{path}</td>
+                          <td>{message}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                  <p>
+                    For further information about resolving these issues, please
+                    refer to the document:{" "}
+                    <a href="https://www.cms.gov/files/document/steps-machine-readable-file.pdf">
+                      8 Steps to a Machine-Readable File
                     </a>
-                  </span>
+                  </p>
                 </>
               )}
-            </Alert>
-            <h3>Errors</h3>
-            <Alert
-              type={valid ? `success` : `error`}
-              aria-live="polite"
-              aria-atomic="true"
-            >
-              {valid ? (
-                <>
-                  <span className="text-bold">No errors found in file</span>:{" "}
-                  <span className="text-underline">{filename}</span>
-                </>
-              ) : (
-                <>
-                  <span className="text-bold">
-                    {errors.length === 1
-                      ? "1 error"
-                      : `${errors.length} errors`}{" "}
-                    found in file
-                  </span>
-                  : <span className="text-underline">{filename}</span>
-                </>
-              )}
-            </Alert>
-            {errors.length > 0 && (
-              <>
+              <h3>Warnings</h3>
+              <Alert type={warnings.length === 0 ? `success` : `warning`}>
+                {warnings.length === 0 ? (
+                  <>
+                    <span className="text-bold">No warnings found in file</span>:{" "}
+                    <span className="text-underline">{filename}</span>
+                  </>
+                ) : (
+                  <>
+                    <span className="text-bold">
+                      {warnings.length === 1
+                        ? "1 warning"
+                        : `${warnings.length} warnings`}{" "}
+                      for file
+                    </span>
+                    : <span className="text-underline">{filename}</span>
+                    <br />
+                    <span>
+                      These items are not required changes, but addressing them
+                      could save time in the future.
+                    </span>
+                  </>
+                )}
+              </Alert>
+              {warnings.length > 0 && (
                 <Table className="width-full" bordered striped>
                   <thead>
                     <tr>
@@ -94,7 +145,7 @@ const ValidationResults = ({
                     </tr>
                   </thead>
                   <tbody>
-                    {errors.map(({ path, message }, index) => (
+                    {warnings.map(({ path, message }, index) => (
                       <tr key={index}>
                         <td>{path}</td>
                         <td>{message}</td>
@@ -102,59 +153,10 @@ const ValidationResults = ({
                     ))}
                   </tbody>
                 </Table>
-                <p>
-                  For further information about resolving these issues, please
-                  refer to the document:{" "}
-                  <a href="https://www.cms.gov/files/document/steps-machine-readable-file.pdf">
-                    8 Steps to a Machine-Readable File
-                  </a>
-                </p>
-              </>
-            )}
-            <h3>Warnings</h3>
-            <Alert type={warnings.length === 0 ? `success` : `warning`}>
-              {warnings.length === 0 ? (
-                <>
-                  <span className="text-bold">No warnings found in file</span>:{" "}
-                  <span className="text-underline">{filename}</span>
-                </>
-              ) : (
-                <>
-                  <span className="text-bold">
-                    {warnings.length === 1
-                      ? "1 warning"
-                      : `${warnings.length} warnings`}{" "}
-                    for file
-                  </span>
-                  : <span className="text-underline">{filename}</span>
-                  <br />
-                  <span>
-                    These items are not required changes, but addressing them
-                    could save time in the future.
-                  </span>
-                </>
               )}
-            </Alert>
-            {warnings.length > 0 && (
-              <Table className="width-full" bordered striped>
-                <thead>
-                  <tr>
-                    <th scope="col">{locationHeader}</th>
-                    <th scope="col">Error description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {warnings.map(({ path, message }, index) => (
-                    <tr key={index}>
-                      <td>{path}</td>
-                      <td>{message}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            )}
-          </>
-        )}
+            </>
+          )}
+        </div>
       </div>
     </Grid>
   )
